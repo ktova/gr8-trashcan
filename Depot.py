dd5d71a30dcaa68bdbcb82e8473976d44c5060f2
ktova <47856123+ktova@users.noreply.github.com>
2019-07-16 17:50:16 +0200

    Add files via upload
    multiple bet choices

--- Depot.py
+++ Depot.py
@@ -9,3 +9,85 @@ Bannounce = "We added a little welcome bonus, here is your new starting bet :"
 RouletteCases = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 
 colorlist = ["red, black, no, Red, Black, No"]
+
+ListofCases = [[1, 'red', 'row1', 'odd', 'col1'], [2, 'black', 'row1', 'even', 'col2'], [3, 'red', 'row1', 'odd', 'col3'], [4, 'black', 'row1', 'even', 'col1'], [5, 'red', 'row1', 'odd', 'col2'], [6, 'black', 'row1', 'even', 'col3'], [7, 'red', 'row1', 'odd', 'col1'], [8, 'black', 'row1', 'even', 'col2'], [9, 'red', 'row1', 'odd', 'col3'], [10, 'black', 'row1', 'even', 'col1'], [11, 'black', 'row1', 'odd', 'col2'], [12, 'red', 'row1', 'even', 'col3'], [13, 'black', 'row2', 'odd', 'col1'], [14, 'red', 'row2', 'even', 'col2'], [15, 'black', 'row2', 'odd', 'col3'], [16, 'red', 'row2', 'even', 'col1'], [17, 'black', 'row2', 'odd', 'col2'], [18, 'red', 'row2', 'even', 'col3'], [19, 'red', 'row2', 'odd', 'col1'], [20, 'black', 'row2', 'even', 'col2'], [21, 'red', 'row2', 'odd', 'col3'], [22, 'black', 'row2', 'even', 'col1'], [23, 'red', 'row2', 'odd', 'col2'], [24, 'black', 'row2', 'even', 'col3'], [25, 'red', 'row3', 'odd', 'col1'], [26, 'black', 'row3', 'even', 'col2'], [27, 'red', 'row3', 'odd', 'col3'], [28, 'black', 'row3', 'even', 'col1'], [29, 'black', 'row3', 'odd', 'col2'], [30, 'red', 'row3', 'even', 'col3'], [31, 'black', 'row3', 'odd', 'col1'], [32, 'red', 'row3', 'even', 'col2'], [33, 'black', 'row3', 'odd', 'col3'], [34, 'red', 'row3', 'even', 'col1'], [35, 'black', 'row3', 'odd', 'col2'], [36, 'red', 'row3', 'even', 'col3']]
+
+rc0 = [0]
+rc1 = [1, 'red', 'row1', 'odd', 'col1',]
+rc2 = [2, 'black', 'row1', 'even', 'col2',]
+rc3 = [3, 'red', 'row1', 'odd', 'col3',]
+rc4 = [4, 'black', 'row1', 'even', 'col1',]
+rc5 = [5, 'red', 'row1', 'odd', 'col2',]
+rc6 = [6, 'black', 'row1', 'even', 'col3',]
+rc7 = [7, 'red', 'row1', 'odd', 'col1',]
+rc8 = [8, 'black', 'row1', 'even', 'col2',]
+rc9 = [9, 'red', 'row1', 'odd', 'col3',]
+rc10 = [10, 'black', 'row1', 'even', 'col1',]
+rc11 = [11, 'black', 'row1', 'odd', 'col2',]
+rc12 = [12, 'red', 'row1', 'even', 'col3',]
+rc13 = [13, 'black', 'row2', 'odd', 'col1',]
+rc14 = [14, 'red', 'row2', 'even', 'col2',]
+rc15 = [15, 'black', 'row2', 'odd', 'col3',]
+rc16 = [16, 'red', 'row2', 'even', 'col1',]
+rc17 = [17, 'black', 'row2', 'odd', 'col2',]
+rc18 = [18, 'red', 'row2', 'even', 'col3',]
+rc19 = [19, 'red', 'row2', 'odd', 'col1',]
+rc20 = [20, 'black', 'row2', 'even', 'col2',]
+rc21 = [21, 'red', 'row2', 'odd', 'col3',]
+rc22 = [22, 'black', 'row2', 'even', 'col1',]
+rc23 = [23, 'red', 'row2', 'odd', 'col2',]
+rc24 = [24, 'black', 'row2', 'even', 'col3',]
+rc25 = [25, 'red', 'row3', 'odd', 'col1',]
+rc26 = [26, 'black', 'row3', 'even', 'col2',]
+rc27 = [27, 'red', 'row3', 'odd', 'col3',]
+rc28 = [28, 'black', 'row3', 'even', 'col1',]
+rc29 = [29, 'black', 'row3', 'odd', 'col2',]
+rc30 = [30, 'red', 'row3', 'even', 'col3',]
+rc31 = [31, 'black', 'row3', 'odd', 'col1',]
+rc32 = [32, 'red', 'row3', 'even', 'col2',]
+rc33 = [33, 'black', 'row3', 'odd', 'col3',]
+rc34 = [34, 'red', 'row3', 'even', 'col1',]
+rc35 = [35, 'black', 'row3', 'odd', 'col2',]
+rc36 = [36, 'red', 'row3', 'even', 'col3',]
+
+Listofcx = []
+
+Listofcx.append(rc1)
+Listofcx.append(rc2)
+Listofcx.append(rc3)
+Listofcx.append(rc4)
+Listofcx.append(rc5)
+Listofcx.append(rc6)
+Listofcx.append(rc7)
+Listofcx.append(rc8)
+Listofcx.append(rc9)
+Listofcx.append(rc10)
+Listofcx.append(rc11)
+Listofcx.append(rc12)
+Listofcx.append(rc13)
+Listofcx.append(rc14)
+Listofcx.append(rc15)
+Listofcx.append(rc16)
+Listofcx.append(rc17)
+Listofcx.append(rc18)
+Listofcx.append(rc19)
+Listofcx.append(rc20)
+Listofcx.append(rc21)
+Listofcx.append(rc22)
+Listofcx.append(rc23)
+Listofcx.append(rc24)
+Listofcx.append(rc25)
+Listofcx.append(rc26)
+Listofcx.append(rc27)
+Listofcx.append(rc28)
+Listofcx.append(rc29)
+Listofcx.append(rc30)
+Listofcx.append(rc31)
+Listofcx.append(rc32)
+Listofcx.append(rc33)
+Listofcx.append(rc34)
+Listofcx.append(rc35)
+Listofcx.append(rc36)
+
+
+print(Listofcx)
